0e5c7b610a38eeb8767ff03234620fe963803ad3
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-14 23:01:04 +0100

    apuntes con menu responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 const rootStyles = document.documentElement.style;
+const menuIconElement = document.getElementById('menu-icon');
+const menuElement = document.getElementById('menu');
+
+const toogleMenu = () => {
+  menuElement.classList.toggle('menu-show');
+};
+
+menuIconElement.addEventListener('click', toogleMenu);
